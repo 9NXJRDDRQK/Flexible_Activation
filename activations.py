d8a2c887a732b926430c821e66038f488fff274e
9NXJRDDRQK <55792087+9NXJRDDRQK@users.noreply.github.com>
2019-09-25 22:19:57 +1000

    Update activations.py

--- activations.py
+++ activations.py
@@ -2,92 +2,46 @@ class para_act_relu(Function):
     
     @staticmethod
     def forward(ctx, input, a, b):
-        
-        # print("a", a)
-        # print("a.shape", a.shape)
+
         ctx.save_for_backward(input, a, b)
-        # output = b * torch.sigmoid(input) + a * torch.relu(input)
-        
-        # output = b * input + a * torch.relu(input)
-        
-        # output_e2 = 0 * input.clone()
-        # output_e2 = input/torch.abs(input) * torch.sqrt(torch.abs(input))
-        
-        # output_e2[input>0] = torch.sqrt(input)
-        # output_e2[input<0] = - torch.sqrt(torch.abs(input))
-        
-        # output = a * torch.relu(input) + b * torch.pow(input, 3) # + (1 - a - b) * output_e2
-        # print("output", output)
         output = a * torch.relu(input) + b * torch.pow(input, 3) + (1 - a - b) * torch.tanh(input)
-        
-        # ctx.save_for_backward(input, a, b, output_e2)
         
         return output
     
     @staticmethod
     def backward(ctx, grad_output):
         
-        # input, a, b, output_e2 = ctx.saved_tensors
         input, a, b = ctx.saved_tensors
         
         grad_a = None
         grad_b = None
         grad_input = grad_output.clone()
         grad_input[input < 0] = 0
-        # grad_output_e2 = 0 * input.clone()
-        # grad_output_e2 = input/torch.abs(input) * (1/2)/torch.sqrt(torch.abs(input))
-        # grad_output_e2[input < 0] = - (1/2)/torch.sqrt(torch.abs(input))
-        # grad_output_e2[input > 0] = (1/2)/torch.sqrt(input)
     
         if ctx.needs_input_grad[0]:
-            # grad_input = b * torch.sigmoid(input) * (1-torch.sigmoid(input)) * grad_output  + a * grad_input
-            # grad_input = b * grad_output + a * grad_input
-            # grad_input = a * grad_input + (3 * b * torch.pow(input, 2) + (1 - a - b) * grad_output_e2) * grad_output
             grad_input = a * grad_input + (3 * b * torch.pow(input, 2) + (1 - a - b) * (1 - torch.tanh(input)**2)) * grad_output
-            # grad_input[:, :] = torch.mean(grad_input, dim = 0)
-         
+               
         if ctx.needs_input_grad[1]:
-            grad_a = (torch.relu(input) - torch.tanh(input)) * grad_output # + lam * (a - torch.mean(a))
-            # grad_a = 200000*torch.relu(input) * grad_output + (a - torch.mean(a))
-            
-            # grad_a = torch.relu(input) * grad_output + 10 * (a - torch.mean(a))
-            # print("grad_a.shape", grad_a.shape)
-            # print("grad_a", grad_a)
-            # grad_a[:, :] = torch.mean(grad_a)
+            grad_a = (torch.relu(input) - torch.tanh(input)) * grad_output # + lam * (a - torch.mean(a))           
             grad_a[:, :] = torch.mean(grad_a, dim = 0)
-            # print("grad_a", grad_a)
-            # print("grad_a.shape", grad_a.shape)
-            # print("grad_a.shape", a.shape)
-            # print("torch.mean(a)", torch.mean(a))
-            # grad_a = (torch.relu(input) - output_e2) * grad_output  # +  (a - torch.mean(a))
-            
+                     
         if ctx.needs_input_grad[2]:
-            grad_b = (torch.pow(input, 3) - torch.tanh(input)) * grad_output # + lam * (b - torch.mean(b))
-            # grad_b = 200000*torch.sigmoid(input) * grad_output + (b - torch.mean(b))
-            # grad_b = input * grad_output + 10 * (b - torch.mean(b))
-            # print("grad_b.shape", grad_b.shape)
-            # grad_b[:, :] = torch.mean(grad_b)
+            grad_b = (torch.pow(input, 3) - torch.tanh(input)) * grad_output # + lam * (b - torch.mean(b))            
             grad_b[:, :] = torch.mean(grad_b, dim = 0)
-            # print("grad_b.shape", grad_b.shape)
-            # print("grad_b.shape", b.shape)
-            # print("torch.mean(b)", torch.mean(b))
-            # grad_b = (torch.pow(input, 3) - output_e2) * grad_output # +  (b - torch.mean(b))
-            
+                     
         return grad_input, grad_a, grad_b
 
 class para_act_sigmoid_tanh(Function):
     
     @staticmethod
     def forward(ctx, input, a, b):
-        # print("forward: self.a, self.b", torch.mean(a.data), torch.mean(b.data))
+        
         act_e2 = b * input.clone() + 1/2
-        act_e2_temp = act_e2.clone()
-        # print("forward: self.a, self.b", torch.mean(a.data), torch.mean(b.data))
+        act_e2_temp = act_e2.clone()        
         act_e2[ act_e2_temp > 1] = 1
         act_e2[ act_e2_temp < 0] = 0
         output = a * torch.sigmoid(input) + (1-a) * act_e2
-        ctx.save_for_backward(input, a, b, act_e2)
-        # print("forward: self.a, self.b", torch.mean(a.data), torch.mean(b.data))
+        ctx.save_for_backward(input, a, b, act_e2)       
 
         return output
     
@@ -99,8 +53,6 @@ class para_act_sigmoid_tanh(Function):
         grad_a = None
         grad_b = None
         
-        # grad_b_e2 = 0 * input.clone()
-        # grad_b_e2[(b * input > 0) and (b * input < 1)] = (1-a) * input
         act_e2_temp = b * input.clone() + 1/2
         grad_b_e2 = (1-a) * input
         grad_b_e2[ act_e2_temp < 0 ] = 0
@@ -125,7 +77,6 @@ class para_act_sigmoid_tanh(Function):
             grad_a = grad_a.type(torch.cuda.FloatTensor)
             grad_b = grad_b.type(torch.cuda.FloatTensor)
             
-        # print("torch.mean(grad_a), torch.mean(grad_b)", torch.mean(grad_a), torch.mean(grad_b))
         return grad_input, grad_a, grad_b
         
 class prelu(Function):
@@ -135,7 +86,6 @@ class prelu(Function):
         
         act_e2 = input.clone()
         act_e2_a = a * input.clone()
-        # print("a.shape, input.shape, act_e2[input < 0].shape", a.shape, input.shape, act_e2[input < 0].shape)
         act_e2[ input < 0] = act_e2_a[ input < 0] # input.clone()
         output = act_e2
         ctx.save_for_backward(input, a, act_e2)
@@ -178,7 +128,7 @@ class p_act_layer_prelu(nn.Module):
         # print("a", torch.mean(self.a))
         return prelu.apply(input, self.a)
 
-class p_act_layer_all(nn.Module):
+class p_act_layer_sig_tanh(nn.Module):
     
     def __init__(self, input_features, layer_type = "sigmoid_slope"):
         
@@ -192,16 +142,13 @@ class p_act_layer_all(nn.Module):
 
         self.a.data = torch.tensor([1.0]*input_features, dtype = torch.float)
         self.b.data = torch.tensor([0.1]*input_features, dtype = torch.float)
-        # print("p_act_layer_sigmoid:__init__:self.a, self.b", torch.mean(self.a.data), torch.mean(self.b.data))
 
     def forward(self, input):
-        # See the autograd section for explanation of what happens here.
-        # print("p_act_layer_sigmoid:forward:self.a, self.b", torch.mean(self.a.data), torch.mean(self.b.data))
+     
         if self.layer_type == "sigmoid_slope":
             return para_act_sigmoid_tanh.apply(input, self.a, self.b), [torch.mean(self.a.data), torch.mean(self.b.data)], [torch.std(self.a.data), torch.std(self.b.data)]
         if self.layer_type == "tanh_slope":
             return para_act_tanh.apply(input, self.a, self.b)
-        # , [torch.mean(self.a.data), torch.mean(self.b.data)], [torch.std(self.a.data), torch.std(self.b.data)]
              
 class p_act_relu(nn.Module):
     
@@ -223,6 +170,5 @@ class p_act_relu(nn.Module):
         self.b.data = torch.tensor([0.0]*input_features, dtype = torch.float)
         
     def forward(self, input):
-        # See the autograd section for explanation of what happens here.
-        # print("self.a, self.b", torch.mean(self.a), torch.mean(self.b))
+
         return para_act_relu.apply(input, self.a, self.b)
